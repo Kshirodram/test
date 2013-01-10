3f13ec9a99b17ec74234dc37615aa65f1e3ccc32
ananyam <ananyam@ananyam-u12.(none)>
2013-01-10 19:44:29 +0530

    2nd commit

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php  
+//Connection string.  test purpose
+$con = mysql_connect("localhost","root","");
+    
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
+
+echo 'Connected successfully';
+
+//Connect/Select the DB.
+mysql_select_db('cake_blog_tutorial');
+
+//Query to be exicuted.
+$query = 'select * from posts';
+
+//Process the query.
+$result = mysql_query($query, $con);
+
+//Array initialization.
+$result_array = array();
+
+//Assign indivisual record to a array.
+while ($row = mysql_fetch_array($result))
+{
+	$result_array[] = $row;
+}
+
+//Print the result.
+print_r($result_array);
+mysql_close($con); 
+?>
